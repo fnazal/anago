c800238ecbed20e3465a54dd93ca38bb24488c76
Fuad <fnazalj18@gmail.com>
2025-11-29 04:57:38 -0300

    Added environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+/* Class that handles excel menu */
+export const environment = {
+    production: false,
+    googleDrive: {
+        menuUrl: 'https://docs.google.com/spreadsheets/d/18lPcdDuFmEi71rtqfqdYJTZ6VcqSucAErMruqmkNa60/export?format=xlsx',
+    }
+};
